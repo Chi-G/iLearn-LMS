77bd480183a54b0d72ad412358abf98c3a492259
Chi-G <chijindu.nwokeohuru@gmail.com>
2024-12-28 20:37:06 +0100

    created a 2nd header component for auth

--- resources/views/frontend/include/header.blade.php
+++ resources/views/frontend/include/header.blade.php
@@ -51,7 +51,7 @@
         <div class="container">
             <div class="td_main_header_in">
                 <div class="td_main_header_left">
-                    <a class="td_site_branding" href="index.html">
+                    <a class="td_site_branding" href="{{ url('/') }}">
                         <img src="{{ asset('frontend/assets/img/logo_v2.svg') }}" alt="Logo">
                     </a>
                 </div>
@@ -60,71 +60,18 @@
                         <div class="td_nav_list_wrap">
                             <div class="td_nav_list_wrap_in">
                                 <ul class="td_nav_list">
-                                    <li class="menu-item-has-children">
+                                    <li>
                                         <a href="index.html">Home</a>
-                                        <ul>
-                                            <li><a href="index.html">University</a></li>
-                                            <li><a href="home-v2.html">Online Educations</a></li>
-                                            <li><a href="home-v3.html">Education</a></li>
-                                            <li><a href="home-v4.html">Kindergarten</a></li>
-                                            <li><a href="home-v5.html">Modern Language</a></li>
-                                            <li><a href="home-v6.html">Al-Quran Learning</a></li>
-                                            <li><a href="home-v7.html">Motivation Speaker</a></li>
-                                            <li><a href="home-v8.html">Kitchen Coach</a></li>
-                                        </ul>
                                     </li>
-                                    <li class="menu-item-has-children">
+                                    <li>
                                         <a href="products.html">Courses</a>
-                                        <ul>
-                                            <li><a href="courses-grid-view.html">Courses Grid View</a></li>
-                                            <li><a href="courses-list-view.html">Courses List View</a></li>
-                                            <li><a href="courses-grid-with-sidebar.html">Courses Grid With
-                                                    Sidebar</a></li>
-                                            <li><a href="course-details.html">Course Details</a></li>
-                                        </ul>
                                     </li>
                                     <li><a href="about.html">About</a></li>
-                                    <li class="menu-item-has-children td_mega_menu">
+                                    <li class="td_mega_menu">
                                         <a href="#">Pages</a>
-                                        <ul class="td_mega_wrapper">
-                                            <li class="menu-item-has-children">
-                                                <h4>Inner Pages</h4>
-                                                <ul>
-                                                    <li><a href="event.html">Upcoming Event</a></li>
-                                                    <li><a href="event-details.html">Event Details</a></li>
-                                                    <li><a href="team-members.html">Team Members</a></li>
-                                                    <li><a href="team-member-details.html">Team Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="menu-item-has-children">
-                                                <h4>Inner Pages</h4>
-                                                <ul>
-                                                    <li><a href="students-registrations.html">Students
-                                                            Registrations</a></li>
-                                                    <li><a href="instructor-registrations.html">Instructor
-                                                            Registrations</a></li>
-                                                    <li><a href="signup.html">Signup</a></li>
-                                                    <li><a href="signin.html">Signin</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="menu-item-has-children">
-                                                <h4>Shop Pages</h4>
-                                                <ul>
-                                                    <li><a href="faqs.html">Faqs</a></li>
-                                                    <li><a href="cart.html">Cart</a></li>
-                                                    <li><a href="checkout.html">Checkout</a></li>
-                                                    <li><a href="error.html">Error</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
                                     </li>
-                                    <li class="menu-item-has-children">
+                                    <li>
                                         <a href="#">Blogs</a>
-                                        <ul>
-                                            <li><a href="blog.html">Blogs</a></li>
-                                            <li><a href="blog-with-sidebar.html">Blog With Sidebar</a></li>
-                                            <li><a href="blog-details.html">Blog Details</a></li>
-                                        </ul>
                                     </li>
                                     <li><a href="contact.html">Contact</a></li>
                                 </ul>
@@ -140,8 +87,8 @@
                                 <span>Sign in</span>
                                 <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M15.1575 4.34302L3.84375 15.6567" stroke="currentColor"
-                                        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M15.1575 4.34302L3.84375 15.6567" stroke="currentColor" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round" />
                                     <path
                                         d="M15.157 11.4142C15.157 11.4142 16.0887 5.2748 15.157 4.34311C14.2253 3.41142 8.08594 4.34314 8.08594 4.34314"
                                         stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
@@ -155,8 +102,8 @@
                                 <span>Sign up</span>
                                 <svg width="19" height="20" viewBox="0 0 19 20" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M15.1575 4.34302L3.84375 15.6567" stroke="currentColor"
-                                        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M15.1575 4.34302L3.84375 15.6567" stroke="currentColor" stroke-width="1.5"
+                                        stroke-linecap="round" stroke-linejoin="round" />
                                     <path
                                         d="M15.157 11.4142C15.157 11.4142 16.0887 5.2748 15.157 4.34311C14.2253 3.41142 8.08594 4.34314 8.08594 4.34314"
                                         stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
