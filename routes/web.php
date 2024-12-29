e771c9832f6308289641151aa340efc5f682803f
Chi-G <chijindu.nwokeohuru@gmail.com>
2024-12-29 11:16:32 +0100

    added students dahsboard

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/student/dashboard', function () {
+    return view('studentdashboard');
+})->middleware(['auth', 'verified'])->name('studentdashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
